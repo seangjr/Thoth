81db70178330093b4d8fb09778c14e7703b708f3
zxlpixl <jasonlzx1306@gmail.com>
2023-02-19 16:12:43 +0800

    register page done

--- client/src/pages/about/index.jsx
+++ client/src/pages/about/index.jsx
@@ -1,15 +1,16 @@
 import { Box } from "@chakra-ui/react";
 
-const aboutus = () => {
-    return (
-      <Box
-        display="flex"
-        flexDirection="column"
-        bgColor="#23262F"
-        overflow="hidden"
-        w = "100vw"
-        h = "100vh"
-      >
-    </Box>
-    )
-}
+const about = () => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      bgColor="#23262F"
+      overflow="hidden"
+      w="100vw"
+      h="100vh"
+    ></Box>
+  );
+};
+
+export default about;
--- client/src/pages/register/index.jsx
+++ client/src/pages/register/index.jsx
@@ -9,7 +9,10 @@ import {
   FormErrorMessage,
   FormHelperText,
   Input,
+  Link,
 } from "@chakra-ui/react";
+
+import { NavLink } from "react-router-dom";
 
 import Logo from "../../assets/tlg.png";
 
@@ -26,29 +29,42 @@ const register = () => {
       alignItems="center"
     >
       <Box>
-        <Image src={Logo} alt="Thoth Logo" mb="20px" />
+        <Image src={Logo} alt="Thoth Logo" mb="20px" boxSize="69px" />
         <Heading size="xl" color="#FCFCFD" mb="20px">
           Sign up
         </Heading>
-        <FormControl mb="15px">
+        <FormControl mb="15px" w={{ lg: "400px", md: "400px", base: "200px" }}>
           <FormLabel size="xs" color="#777E90">
             EMAIL:
           </FormLabel>
           <Input type="email" color="#777E90" />
         </FormControl>
-        <FormControl mb="15px">
+        <FormControl mb="15px" w={{ lg: "400px", md: "400px", base: "200px" }}>
           <FormLabel size="xs" color="#777E90">
             USERNAME:
           </FormLabel>
           <Input color="#777E90" />
         </FormControl>
-        <FormControl mb="15px">
+        <FormControl mb="15px" w={{ lg: "400px", md: "400px", base: "200px" }}>
           <FormLabel size="xs" color="#777E90">
             PASSWORD:
           </FormLabel>
           <Input type="password" color="#777E90" />
         </FormControl>
-        <Button colorScheme="gray">Get Started</Button>
+        <Button
+          mt="10px"
+          mb="10px"
+          colorScheme="gray"
+          w={{ lg: "400px", md: "400px", base: "200px" }}
+        >
+          Sign up
+        </Button>
+        <Text mb="20px" color="#777E90" w="100%">
+          Already have an account?{" "}
+          <Link color="#5E6EFF" as={NavLink} to="/">
+            Log in
+          </Link>
+        </Text>
       </Box>
     </Box>
   );
