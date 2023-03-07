09d14c4195d764b8f665de08bae9909d5789ff5c
seangjr <sean@relampagos.org>
2023-03-07 23:41:05 +0800

    deleted unused deps

--- client/src/pages/feed/index.jsx
+++ client/src/pages/feed/index.jsx
@@ -1,11 +1,4 @@
-import {
-    Box,
-    Container,
-    Heading,
-    Spinner,
-    Avatar,
-    Badge,
-} from "@chakra-ui/react";
+import { Box, Container, Heading, Spinner } from "@chakra-ui/react";
 import { useState } from "react";
 import { useAuth } from "../../contexts/AuthContext";
 import ListItem from "../../components/feed/ListItem";
